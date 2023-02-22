caf37e7a06aa5b6e038c99168b68bfbe4c2c3236
SadeJoseph <115725845+SadeJoseph@users.noreply.github.com>
2023-02-22 21:13:53 +0000

    Add files via upload

---
+++ challenge (5)/starter/app.js
@@ -0,0 +1,24 @@
+//moment js
+
+
+let timeNow = moment().format("HH");
+const currentDay = $("#currentDay");
+const container = $(".container");
+let taskArray =[]
+
+
+
+let ul;
+let button;
+let textbox;
+let li;
+let hourSect;
+
+
+console.log(timeNow)
+//Day - current display 
+ displayCurrentDay()
+ timeBlock()
+
+
+ 

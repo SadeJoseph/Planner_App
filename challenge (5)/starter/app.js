5abbbce1a32220f9cd5f9cf5b958e31c003d1acb
SadeJoseph <feojoseph1@gmail.com>
2023-02-22 21:27:58 +0000

    cuurent day display

--- challenge (5)/starter/app.js
+++ challenge (5)/starter/app.js
@@ -20,5 +20,18 @@ console.log(timeNow)
  displayCurrentDay()
  timeBlock()
 
+ function displayCurrentDay () {
+    currentDay.text(moment().format("dddd,MMMM Do"))
+ }
+
+ //time blcoks
+
+ function timeBlock (){
+    var displayTime = "09 AM";
+    var icon;
+    ul = #("ul").addClass("time-block")
+    container.append(ul)
+ }
+
 
  
